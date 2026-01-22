f5bdf4be66aaeb0330e1fd0433251c8c96351e82
Jason <jtritton@hotmail.com>
2026-01-22 08:05:31 +0000

    Update ed047tc1.cpp

--- components/ed047tc1/ed047tc1.cpp
+++ components/ed047tc1/ed047tc1.cpp
@@ -204,15 +204,11 @@ void ED047TC1Display::dump_config() {
     LOG_PIN("  XLE_PIN (LEH): ", xle_pin_); LOG_PIN("  SPV_PIN (STV): ", spv_pin_);
     LOG_PIN("  CKV_PIN (CKV): ", ckv_pin_);
     for (int i=0; i<8; ++i) {
-        #if ESPHOME_VERSION_CODE >= VERSION_CODE(2023, 1, 0)
         if (d_pins_[i] != nullptr) {
-          ESP_LOGCONFIG(TAG, "  Data Pin %d: GPIO%d", i, d_pins_[i]->get_pin());
-        } else if (d_pins_[i] != nullptr) {
-            ESP_LOGCONFIG(TAG, "  D%d Pin: Configured (Pin: %d)", i, esphome_pin_to_gpio_num(d_pins_[i]));
-        } else { ESP_LOGCONFIG(TAG, "  D%d Pin: NONE", i); }
-        #else
-        ESP_LOGCONFIG(TAG, "  D%d Pin: %s", i, d_pins_[i] ? "Configured" : "NONE");
-        #endif
+            ESP_LOGCONFIG(TAG, "  D%d Pin: GPIO%d", i, esphome_pin_to_gpio_num(d_pins_[i]));
+        } else {
+            ESP_LOGCONFIG(TAG, "  D%d Pin: NONE", i);
+        }
     }
     LOG_UPDATE_INTERVAL(this);
 }
@@ -220,3 +216,4 @@ void ED047TC1Display::dump_config() {
 }  // namespace ed047tc1
 
 }  // namespace esphome
+
